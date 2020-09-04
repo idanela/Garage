b6dd6e3fc615ed3ed7ba6ba879e02d941584d7e4
erez <erezlev1994@gmail.com>
2020-09-05 00:32:12 +0300

    Start implement Wheel, Engine classes.

--- Garage/Engine.cs
+++ Garage/Engine.cs
@@ -1,13 +1,50 @@
 ﻿
+using System;
+
 namespace Garage
 {
     public abstract class Engine
     {
+        // Data Member:
+        protected float m_CurrentAmountOfEnergy;
+        protected readonly float m_MaxCapacityEnergy;
+
         // Enums:
         public enum eEngineType
         {
             Gas = 0,
-            Electric = 1
+            Electric
         }
+
+        // Constructors:
+        protected Engine(float iCurrentCapacityEnergy, float i_MaxCapacityEnergy)
+        {
+            m_CurrentAmountOfEnergy = iCurrentCapacityEnergy;
+            m_MaxCapacityEnergy = i_MaxCapacityEnergy;
+        }
+
+        // Properties:
+        protected float CurrentCapacityEnergy
+        {
+            get
+            {
+                return m_CurrentAmountOfEnergy;
+            }
+            set
+            {
+                m_CurrentAmountOfEnergy = value;
+            }
+        }
+
+        protected float MaxCapacityEnergy
+        {
+            get
+            {
+                return m_MaxCapacityEnergy;
+            }
+        }
+
+        // Methods:
+        protected abstract void FillUpEnergy(float i_EnergyAmountToFill, string i_GasType);
     }
 }
