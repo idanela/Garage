d719ce8bc84b9dd2fb67683e08abc413185c60df
erez <erezlev1994@gmail.com>
2020-09-06 20:22:52 +0300

    Most changes in UI.

--- Garage/ValueOutOfRangeException.cs
+++ Garage/ValueOutOfRangeException.cs
@@ -2,7 +2,7 @@
 
 namespace Ex03.GarageLogic
 {
-    class ValueOutOfRangeException : Exception
+    public class ValueOutOfRangeException : Exception
     {
         // Data Memmbers:
         private readonly float r_MaxValue;
